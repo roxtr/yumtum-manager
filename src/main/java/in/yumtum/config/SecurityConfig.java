f942cbde71f485c029ba02b1df6b5d47e84502a6
hareesh-roxtr <hareesh@roxtr.com>
2012-09-05 09:05:12 +0530

    new files.

---
+++ src/main/java/in/yumtum/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package in.yumtum.config;
+
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ImportResource(value = "classpath:spring-security-context.xml")
+public class SecurityConfig {}
