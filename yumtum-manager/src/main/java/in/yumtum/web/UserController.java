377bd18764a81afb434ca6fd6ed55989cb86c4ef
hareesh-roxtr <hareesh@roxtr.com>
2012-09-05 08:54:19 +0530

    Initial setup for yumtum-manager

---
+++ yumtum-manager/src/main/java/in/yumtum/web/UserController.java
@@ -0,0 +1,24 @@
+package in.yumtum.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("user")
+@Secured("ROLE_USER")
+public class UserController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+	
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String index(UserDetails userDetails, Model model) {
+		LOG.info(userDetails.toString());
+		return "user/index";
+	}
+}
